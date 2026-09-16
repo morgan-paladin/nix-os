c707456e0294e502bcbf43e163c0fa32d97188a9
morgan-paladin <morgan.paladin@proton.me>
2026-09-16 20:36:08 +0100

    Added configuration to work with virtual machine

--- configuration.nix
+++ configuration.nix
@@ -2,10 +2,12 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    (builtins.fetchTarball {
+      url = "https://github.com/nix-community/home-manager/archive/release-26.05.tar.gz";
+    } + "/nixos")
+  ];
 
   # Use the grub EFI boot loader.
   boot.loader.grub.enable = true;
@@ -32,17 +34,26 @@
   environment.shells = [ pkgs.zsh ];
   
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.morgan = {
+  users.users.morgan = {
      isNormalUser = true;
      extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
        tree
      ];
      shell = pkgs.zsh;
-   };
+  };
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;   
+  home-manager.users.morgan = import ./home.nix;
+  home-manager.backupFileExtension = "backup";
 
   # Enable docker
   virtualisation.docker.enable = true;
+
+  # VM Virtualisation
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
 
   # programs.firefox.enable = true;
 
@@ -53,17 +64,47 @@
      curl
      micro
      fastfetch
-     git
      btop
      fd
      fzf
      eza
      jq
+     hyprland
+     virt-manager
+     unzip
+     noctalia
    ];
+
+  programs.hyprland = { # or wayland.windowManager.hyprland
+    enable = true;
+    xwayland.enable = true;
+  }; 
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.ubuntu
+    nerd-fonts.ubuntu-mono
+  ];   
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+  # Greetd
+  services.greetd = {
+  enable = true;
+  settings = {
+    default_session = {
+      # Specify the greeter and the session command (e.g., sway, hyprland)
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
+      user = "greeter";
+    };
+  };
+  };   
+
+
+  nix.settings.experimental-features = [ "nix-command" ];
   system.stateVersion = "26.05";
 }
 
--- home.nix
+++ home.nix
@@ -3,9 +3,7 @@
 {
   home.username = "morgan";
   home.homeDirectory = "/home/morgan";
-  programs.git.enable = true;
   home.stateVersion = "26.05";
-
 
   programs.zsh = {
       enable = true;
@@ -13,29 +11,60 @@
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       shellAliases = {
-      	btw = "echo i use nixos-btw";
-      	nrs = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
-	      ld = "lazydocker";
-	      ls = "eza --icons -1";
-	      c = "clear";
-      	z = "zoxide";
+      lg = "lazygit";
+      nrs = "sudo nixos-rebuild switch";
+	    ld = "lazydocker";
+	    ls = "eza --icons -1";
+	    c = "clear";
     	};
     };
-  
+ 
+  programs.git = {
+  	enable = true;
+
+  	settings = {
+    	user = {
+      	name = "morgan-paladin";
+      	email = "morgan.paladin@proton.me";
+    	};
+
+    init.defaultBranch = "main";
+    pull.rebase = false;
+    push.autoSetupRemote = true;
+
+  		};
+	}; 
   home.packages = with pkgs; [
-    neovim
-    zoxide
     ripgrep
     nil
     nixpkgs-fmt
     python3
     gcc
+    git
     lazydocker
     lazygit
+    vscodium
+    waybar
+    kitty
   ];
+
+  programs.brave.enable = true;
   
   programs.zoxide = {
-	
-	enableZshIntegration = true;
+  	enable = true;
+	  enableZshIntegration = true;
   };
+  programs.starship = {
+  	enable = true;
+  	enableZshIntegration = true;
+  	settings = {
+  		command_timeout = 1300;
+  		scan_timeout = 50;
+  	};
+  };
+
+  programs.neovim = {
+  enable = true;
+};
+
 }
